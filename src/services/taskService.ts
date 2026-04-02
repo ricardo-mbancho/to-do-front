a133ce081c0cb87c22d763995eaf4e42b22177e6
unknown <fkondem@gmail.com>
2026-04-02 20:54:15 +0200

    feat: add vuex state

--- src/services/taskService.ts
+++ src/services/taskService.ts
@@ -1,9 +1,9 @@
-import axios from 'axios'
+import axios from 'axios';
 
 const api = axios.create({
   baseURL: process.env.VUE_APP_API_URL
-})
+});
 
-export const fetchTasks = () => api.get('/tasks')
+export const fetchTasks = () => api.get('/tasks');
 export const createTask = (title: string) =>
   api.post('/tasks', { title })
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,66 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue';
+import Vuex from 'vuex';
+import axios from 'axios';
+import { createTask, fetchTasks } from '@/services/taskService';
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
-});
+export interface Task {
+  _id: string
+  title: string
+}
+
+interface State {
+  tasks: Task[],
+  error: string;
+}
+const customeError = {
+  loadedTasks: 'Failed to load Todos, Please try again in few seconds',
+  addTask: 'Failed to add Todos, Please try again in few seconds',
+}
+
+
+export default new Vuex.Store<State>({
+  state: {
+    tasks: [],
+    error: ""
+  },
+
+  mutations: {
+    SET_TASKS(state, tasks: Task[]) {
+      state.tasks = tasks
+    },
+    ADD_TASK(state, task: Task) {
+      state.tasks.unshift(task)
+    },
+    SET_ERROR(state, message) {
+      state.error = message
+    },
+  },
+
+  actions: {
+    async loadTasks({ commit }) {
+      try {
+        const res = await fetchTasks();
+        commit('SET_TASKS', res.data);
+        commit('SET_ERROR', '');
+      } catch(err) {
+        commit('SET_ERROR', customeError.loadedTasks);
+      }
+    },
+    async addTask({ commit }, title: string) {
+      try {
+        const res = await createTask(title);
+        commit('ADD_TASK', res.data);
+        commit('SET_ERROR', '');
+      } catch(err) {
+        commit('SET_ERROR', customeError.addTask);
+      }
+    }
+  },
+
+  getters: {
+    tasks: (state) => state.tasks,
+    errorMessage: (state) => state.error
+  }
+})
