e26900090f9cb15c184e299084a9401ca79f27ed
unknown <fkondem@gmail.com>
2026-04-02 21:13:48 +0200

    feat: add HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,54 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <TodoInput />
+  <div>
+    <TodoHeader title="To-Do List with Real-Time Updates" />
+    <TodoInput @add="handleAdd" />
+    <TodoList :tasks="tasks" :errorMessage="errorMessage" />
   </div>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import TodoInput from "@/components/TodoInput.vue";
+  import Vue from 'vue';
+  import TodoHeader from '../components/TodoHeader.vue';
+  import TodoInput from '../components/TodoInput.vue';
+  import TodoList from '../components/TodoList.vue';
 
-export default Vue.extend({
-  name: "HomeView",
-  components: {
-    TodoInput,
-  },
-});
+  export default Vue.extend({
+    name: 'HomeView',
+    components: { TodoHeader, TodoInput, TodoList },
+    data() {
+      return {
+        interval: null as ReturnType<typeof setInterval> | null
+      }
+    },
+
+    computed: {
+      tasks() {
+        return this.$store.getters.tasks
+      },
+      errorMessage() {
+        return this.$store.getters.errorMessage;
+      }
+    },
+
+    methods: {
+      handleAdd(title: string) {
+        this.$store.dispatch('addTask', title)
+      }
+    },
+
+    created() {
+      this.$store.dispatch('loadTasks');
+
+      // Poll tasks every 5 seconds
+      this.interval = setInterval(() => {
+        this.$store.dispatch('loadTasks');
+      }, 5000)
+    },
+
+    beforeDestroy() {
+      if (this.interval) {
+        clearInterval(this.interval)
+      }
+    }
+  })
 </script>
