5df448a0b04b776ad88d79bad0dff3326d256560
unknown <fkondem@gmail.com>
2026-04-02 20:50:02 +0200

    feat: add Todo Header component

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+
+  export default Vue.extend({
+    props: {
+      title: {
+        type: String,
+        required: true
+      }
+    }
+  })
+</script>
+
+<style scoped>
+  header {
+    text-align: center;
+    margin-bottom: 1rem;
+  }
+</style>
